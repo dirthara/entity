d474193f42df46e9edcad87cb0cdf50cf48b8c8f
bricknpc <bricknpc@proton.me>
2026-09-12 19:43:25 +0200

    Use property names when reporting a composite identifier

--- src/Exception/InvalidIdentifierException.php
+++ src/Exception/InvalidIdentifierException.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Dirthara\Entity\Exception;
 
+use Dirthara\Entity\Metadata\PropertyMetadata;
+
 class InvalidIdentifierException extends EntityException
 {
     public static function compositeExpected(string $entity): self
@@ -13,13 +15,18 @@ class InvalidIdentifierException extends EntityException
         ]);
     }
 
+    /**
+     * @param non-empty-list<PropertyMetadata> $properties
+     */
     public static function identifierIsComposite(array $properties): self
     {
+        $names = array_map(static fn(PropertyMetadata $property): string => $property->property, $properties);
+
         return new self(sprintf('The entity has a composite identifier while a single identifier is expected: %s', implode(
             ', ',
-            $properties,
+            $names,
         )))->addContext([
-            'properties' => $properties,
+            'properties' => $names,
         ]);
     }
 
